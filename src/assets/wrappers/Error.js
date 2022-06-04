9033a5d5256eb52d175145b5fa0be3cc6ccb4da3
JaneMoroz <moroz.jane@yahoo.com>
2022-06-04 15:49:47 +0300

    Added Error Page

---
+++ src/assets/wrappers/Error.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3.2rem;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  p {
+    padding-bottom: 2rem;
+  }
+
+  img {
+    width: 100%;
+    max-height: 50rem;
+  }
+`;
+
+export default Wrapper;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/Error";
+import img from "../assets/images/error-image.svg";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="container--max">
+      <h2>Ohh! Page not found</h2>
+      <p>We can't seem to find the page you're looking for!</p>
+      <Link className="btn btn--outlined" to="/">
+        back home
+      </Link>
+      <img src={img} alt="Cat and Dog" />
+    </Wrapper>
+  );
 };
 
 export default Error;
